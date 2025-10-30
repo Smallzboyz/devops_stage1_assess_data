4220d5abcacbd0235ea1c73f511b781d493cc947
Onyegbula Royal <onyegbularoyal@gmail.com>
2025-10-30 16:19:28 +0100

    my new update

--- create_users.sh
+++ create_users.sh
@@ -1,7 +1,15 @@
 #!/bin/bash 
 
+
 read -p "Enter your Firstname: " Firstname
 
 read -p "Enter your Lastname: " Lastname
 
-echo "Welcome, $users!"
+read -p "Enter your Email address: " Email address
+
+read -p "Enter your Password: " password
+
+user=("$Firstname" "$Lastname" "$Email_address" "$password")
+echo "${user[@]}"
+
+echo "Welcome, ${users[@]} ${user[0]} ${user[1]}!"
